02463ae53671b2f8de9834f34cfa2e51cbdd2d1d
Juan Alvarez <JuanAlvarez@MacBook-Pro-de-Triario.local>
2023-02-15 22:28:03 -0500

    Esto esta raro

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './styles/App.css';
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Home/>} />
+				</Routes>
+		</BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../styles/Home.css';
+
+const Home = () => {
+	return (
+	<div className="home">
+      <header className="Home-header">
+        <div>Esto esta raro</div>
+      </header>
+    </div>
+	);
+}
+
+export default Home;
