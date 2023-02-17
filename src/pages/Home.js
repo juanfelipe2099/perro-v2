87901f835f1d86ea32a8762cbec50d488204f97c
Juan Alvarez <JuanAlvarez@MacBook-Pro-de-Triario.local>
2023-02-16 22:58:36 -0500

    Home botones

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,30 @@
 import React from 'react'
-import '../styles/Home.css';
+import '../styles/Home.scss';
 
 const Home = () => {
 	return (
-	<div className="home">
-      <header className="Home-header">
-        <div>Esto esta raro</div>
-      </header>
+    <div>
+      <div className="container">
+      <div className="image">
+        <img src="http://www.tucachorroideal.com.co/wp-content/uploads/2016/03/collie-2.jpg" />
+        <div className="text">Perritos</div>
+      </div>
+      <div className="image">
+       <img src="https://gatoanimal.com/wp-content/uploads/2020/04/gato-persa-silver-1-560x560.jpg" />
+      <div className="text">Gaticos</div>
+     </div>
+     <div className="image">
+       <img src="https://i.pinimg.com/1200x/6a/49/3e/6a493ec16b2b4a44bd703bc7f3d5dd4b.jpg" />
+      <div className="text">Gaticos</div>
+     </div>
     </div>
+    </div>
+    
+    
+
+
+  
+    
 	);
 }
 
